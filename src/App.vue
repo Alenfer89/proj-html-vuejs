4b6cd6687b4d59f6cc98414682019762e2bdfc02
Alenfer89 <alberioalex@gmail.com>
2022-04-07 12:56:42 +0200

    fonts-and-style-folder

--- src/App.vue
+++ src/App.vue
@@ -17,8 +17,9 @@ export default {
 </script>
 
 <style lang="scss">
+@import './assets/styles/style.scss';
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
