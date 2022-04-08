e652c35b3da8f3e20beec904a33ec00c29d297f3
Alenfer89 <alberioalex@gmail.com>
2022-04-08 18:07:34 +0200

    bollean-vlaues-somehow-recovered

---
+++ src/components/BlogPost.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="ax-blog-post d-flex justify-content-between rounded-3 text-white"
+    :class="(focus === true) ? 'col-6 ' : 'col-3' ">
+        <div>
+            <p> {{ focus }} </p>
+            <p> {{ date }} / {{ author }} </p>
+            <h3> {{ title }} </h3>
+            <p> {{ article }} </p>
+        </div>
+        <button>Read More</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BlogPost',
+    props:{
+        title: String,
+        article: String,
+        author: String,
+        date: String,
+        focus: Boolean
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../assets/styles/style.scss';
+
+div.ax-blog-post{
+    height: 400px;
+    background-color: rgb(22, 22, 115);
+}
+</style>
--- src/components/SectionBlog.vue
+++ src/components/SectionBlog.vue
@@ -8,18 +8,61 @@
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus quis natus eum architecto soluta quisquam consequuntur, reiciendis minima laboriosam atque omnis explicabo.</p>
                 </div>
             </div>
+            <div class="row">
+                <BlogPost
+                v-for='(blog, index) in blogNews'
+                :key='index'
+                :title='blog.title'
+                :article='blog.text'
+                :author='blog.author'
+                :date='blog.date'
+                :focus='blog.latest' />
+            </div>
         </div>
     </section>
 </template>
 
 <script>
+import BlogPost from './BlogPost.vue';
+
 export default {
     name: 'SectionBlog',
+    components:{
+        BlogPost,
+    },
+    data: function(){
+        return{
+            blogNews:[
+                {
+                    title:'Amazon get CCI nod to acquire 49% stake in',
+                    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis dignissimos dicta consequuntur numquam temporibus asperiores deleniti veniam vel consequatur...',
+                    author: 'Paul',
+                    date: 'April 08, 2022',
+                    latest: true
+                },
+                {
+                    title:'Clearing corporations get SEBI nod to invest in overnight funds',
+                    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis dignissimos dicta consequuntur numquam temporibus asperiores deleniti veniam vel consequatur...',
+                    author: 'Paul',
+                    date: 'April 08, 2022',
+                    latest: false
+                },
+                {
+                    title:'Shabnam Dhillion, wife of RSSB chief Gurinder Dhillon, passes away in London',
+                    text:'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga facilis dignissimos dicta consequuntur numquam temporibus asperiores deleniti veniam vel consequatur...',
+                    author: 'Paul',
+                    date: 'April 08, 2022',
+                    latest: false
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style lang='scss' scoped>
 @import '../assets/styles/style.scss';
+
 section#blog{
 
     p:first-child{
