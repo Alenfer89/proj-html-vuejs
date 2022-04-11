0f189883e59260e15f7ff7bac8500292f89be6d6
Alenfer89 <alberioalex@gmail.com>
2022-04-11 16:10:48 +0200

    bot-footer-completed

--- src/components/FooterSubComps/BotFooter.vue
+++ src/components/FooterSubComps/BotFooter.vue
@@ -1,11 +1,21 @@
 <template>
-    <nav class="d-flex justify-content-between align-items-center">
-        <p>
-            © 2022 phlox. All rights reserved.
-        </p>
-        <div>
-
+    <nav>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between align-items-center py-5">
+                    <p class="m-0">
+                        © 2022 phlox. All rights reserved.
+                    </p>
+                    <div>
+                        <i class="fa-brands fa-instagram"></i>
+                        <i class="fa-brands fa-linkedin-in"></i>
+                        <i class="fa-brands fa-facebook-f"></i>
+                        <i class="fa-brands fa-twitter"></i>
+                    </div>
+                </div>
+            </div>
         </div>
+        
     </nav>
 </template>
 
@@ -20,6 +30,11 @@ export default {
 
 nav{
     border-top: 1px solid rgba(255, 255, 255, 0.5);
+    i{
+        color: white;
+        display: inline-block;
+        margin-left: 3rem;
+    }
 }
 
 </style>
