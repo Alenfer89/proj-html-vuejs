a9e759b71bed111343ba549564761636b0eb6ddd
Alenfer89 <alberioalex@gmail.com>
2022-04-11 12:37:21 +0200

    reviews-section-completed

---
+++ src/components/ReviewCard.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="col text-center">
+        <div class="ax-image-wrapper mb-5">
+            <img class="img-fluid" :src="require(`../assets/images/${image}`)" :alt="name +' profile pic'">
+        </div>
+        <div class="mb-5">
+            <p class="mb-4">
+                <i class="fa-solid fa-star"
+                v-for='(element, index) in rating'
+                :key='index'>
+                </i>
+                <i class="fa-regular fa-star"
+                v-for='(element, index) in 5-rating'
+                :key='index'>
+                </i>
+            </p>
+            <p> {{ text }} </p>
+        </div>
+        <div>
+            <h6 class="ax-bold"> {{ name }} </h6>
+            <p> {{ role }} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+    name: 'ReviewCard',
+    props:{
+        name:String,
+        role:String,
+        text:String,
+        image:String,
+        rating:Number
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../assets/styles/style.scss';
+div.ax-image-wrapper{
+    width: 110px;
+    height: 110px;
+    margin: 0 auto;
+    border-radius: 50%;
+    border: 4px solid $secondaryColor;
+}
+i.fa-solid.fa-star{
+    @include bgGradientTopToBottom ($secondaryColor , $secondaryLightColor);
+    background-clip: text;
+    color: transparent;
+    //-webkit-text-fill-color: transparent;
+}
+i.fa-regular.fa-star{
+    @include bgGradientTopToBottom ($secondaryColor , $secondaryLightColor);
+    background-clip: text;
+    color: transparent;
+    //-webkit-text-fill-color: transparent;
+}
+
+</style>
--- src/components/SectionTestimonial.vue
+++ src/components/SectionTestimonial.vue
@@ -1,29 +1,73 @@
 <template>
     <section id="testimonial" class="py-5">
         <div class="container">
-            <div class="row py-5">
+            <div class="row py-5 mb-5">
                 <div class="col-6 me-auto ms-auto text-center">
                     <p class="text-capitalize">testimonial</p>
                     <h2 class="text-capitalize mb-3"><span class="ax-bold">what</span> people say</h2>
                     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus quis natus eum architecto soluta quisquam consequuntur, reiciendis minima laboriosam atque omnis explicabo.</p>
                 </div>
             </div>
-            <div class="row row-cols-3">
-
+            <div class="row row-cols-3 p-4">
+                <Review v-for="(review, index) in reviewsList"
+                :key='index'
+                :name='review.name'
+                :role='review.role'
+                :text='review.text'
+                :image='review.image'
+                :rating='review.rating'/>
             </div>
-            <div class="row row-cols-5">
-
+            <div class="row row-cols-5 py-5">
+                <div class="col text-center py-5"
+                v-for='(client, index) in clientsList'
+                :key="index">
+                    <img :src="require(`../assets/images/${client}`)" alt="">
+                </div>
             </div>
         </div>
     </section>
 </template>
 
 <script>
+import Review from './ReviewCard.vue';
+
 export default {
     name: 'SectionTestimonial',
+    components:{
+        Review,
+    },
     data: function(){
         return{
-            
+            reviewsList:[
+                {
+                    name: 'Michkel Anegl',
+                    role: 'Marketing Management',
+                    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, dolorum nihil nisi ipsum hic repellendus voluptatum vel nesciunt repudiandae deserunt.',
+                    rating: 3,
+                    image: 'philipe-cavalcante-539505-unsplash-1024x1024.png'
+                },
+                {
+                    name: 'Michkel Anegl',
+                    role: 'Marketing Management',
+                    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, dolorum nihil nisi ipsum hic repellendus voluptatum vel nesciunt repudiandae deserunt.',
+                    rating: 3,
+                    image: 'szabo-viktor-1266895-unsplash-1024x1024.png'
+                },
+                {
+                    name: 'Michkel Anegl',
+                    role: 'Marketing Management',
+                    text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, dolorum nihil nisi ipsum hic repellendus voluptatum vel nesciunt repudiandae deserunt.',
+                    rating: 3,
+                    image: 'fabio-spinelli-695744-unsplash-1024x1024.png'
+                }
+            ],
+            clientsList:[
+                'client-1-1.png',
+                'clienty-2.png',
+                'clienty-3.png',
+                'clienty-4.png',
+                'client-5.png'
+            ]
         }
     }
 }
