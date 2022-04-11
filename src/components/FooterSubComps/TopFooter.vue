6f8ea38343bb0534372efd3498e8b5424caefe10
Alenfer89 <alberioalex@gmail.com>
2022-04-11 13:30:19 +0200

    footer-banner-positioned

--- src/components/FooterSubComps/TopFooter.vue
+++ src/components/FooterSubComps/TopFooter.vue
@@ -1,12 +1,15 @@
 <template>
-    <section class="position-relative">
-        <div id="footer-banner" class="container">
-            <div class="row">
-                <div class="col">
-                    <h1>subscribe</h1>
+    <section class="position-relative mt-5">
+        <div id="footer-tag" class="position-relative">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1>subscribe</h1>
+                    </div>
                 </div>
             </div>
         </div>
+        
         <div class="container">
             <div class="row">
                 <div class="col-12">
@@ -26,11 +29,14 @@ export default {
 <style lang='scss' scoped>
 @import '../../assets/styles/style.scss';
 
-div#footer-banner{
+div#footer-tag{
     top: 0;
     left: 0;
     transform: translateY(-50%);
-    @include bgGradientLeftToRight ($secondaryColor , $secondaryLightColor)
+    div.col{
+        @include bgGradientLeftToRight ($secondaryColor , $secondaryLightColor);
+        height: 12rem;
+    }
 }
 
 </style>
