e4f227c72f361e7a5b5633e14cac9ae78610d5d1
Alenfer89 <alberioalex@gmail.com>
2022-04-11 17:16:38 +0200

    carousel-not-working-solution

---
+++ src/components/CarouselCard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="ax-carousel-element">
+        <div class="img-wrapper">
+            <img :src="require(`../assets/images/${thumb}`)" :alt="name">
+        </div>
+        <div class="d-flex justify-content-between align-items-center">
+            <h6 class="ax-bold"> {{ name }} </h6>
+            <p> {{ category }} </p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CarouselCard',
+    props:{
+        name: String,
+        category: String,
+        thumb: String
+    }
+}
+</script>
+
+<style lang='scss' scoped>
+@import '../assets/styles/style.scss';
+div.ax-carousel-element{
+    width: calc(100% - 5rem) / 3;
+    border-radius: 2rem;
+
+    div.img-wrapper{
+        border-radius: 2rem;
+        width: 100%;
+        img{
+            width: 100%;
+        }
+    }
+}
+</style>
--- src/components/SectionPortfolio.vue
+++ src/components/SectionPortfolio.vue
@@ -1,17 +1,109 @@
 <template>
-    <div>
-        <h1>
-            portfolio
-        </h1>
-    </div>
+    <section id="portfolio" class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between align-items-center py-5">
+                    <div>
+                        <p class="text-capitalize">our services</p>
+                        <h2 class=" mb-3"><span class="ax-bold">latest</span> work</h2>
+                    </div>
+                    <div class="d-flex">
+                        <div class="ax-swapper me-3">
+                            <i class="fa-solid fa-arrow-left-long"></i>
+                        </div>
+                        <div class="ax-swapper">
+                            <i class="fa-solid fa-arrow-right-long"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12 d-flex">
+                    <CarouselCard
+                    v-for='(element, index) in worksList'
+                    :key='index'
+                    :name='element.name'
+                    :category='element.category'
+                    :thumb='element.thumbnail' />
+                </div>
+                <div class="col-12"></div>
+            </div>
+        </div>
+        
+    </section>
 </template>
 
 <script>
+import CarouselCard from './CarouselCard.vue';
+
 export default {
-    name: 'SectionPortfolio'
+    name: 'SectionPortfolio',
+    components:{
+        CarouselCard,
+    },
+    data: function(){
+        return{
+            worksList:[
+                {
+                    name: 'Purinky Products',
+                    category: 'Digital Experience',
+                    thumbnail: 'DRY-1-790x576.jpg'
+                },
+                {
+                    name: 'Satisfy Poster',
+                    category: 'Branding Strategy',
+                    thumbnail: 'a247b00b-3621-470f-b4b8-b3ac46f25907-1-790x576.jpg'
+                },
+                {
+                    name: 'Mock-Up',
+                    category: 'something-else',
+                    thumbnail: '84316050-0af0-49db-a53a-241d47ddad0e-2-790x576.jpg'
+                },
+                {
+                    name: 'Something',
+                    category: 'Digital Experience',
+                    thumbnail: 'a247b00b-3621-470f-b4b8-b3ac46f25907-1-790x576.jpg'
+                },
+                {
+                    name: 'Basket of flower on table',
+                    category: 'Branding Strategy',
+                    thumbnail: '8wa60okr-1-790x576.jpg'
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <style lang='scss' scoped>
+@import '../assets/styles/style.scss';
 
+section#portfolio{
+    p:first-child{
+        color: $secondaryLightColor;
+    }
+
+    div.ax-swapper{
+        width: 40px;
+        height: 40px;
+        line-height: 40px;
+        text-align: center;
+        border-radius: 50%;
+        border: 1px solid $secondaryLightColor;
+        color: $secondaryLightColor;
+        &:hover{
+            cursor: pointer;
+        }
+    }
+    div.ax-displaced-carousel{
+        left: -500px;
+        right: -500px;
+    }
+    div.placeholding-el{
+        width: 400px;
+        height: 300px;
+        margin-right: 2rem;
+        background-color: aquamarine;
+    }
+}
 </style>
